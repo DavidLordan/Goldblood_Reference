2b5a02337c6f5efe1578b88539bcb56b3e30220a
davidlordan <david_lordan@student.uml.edu>
2014-12-31 20:51:00 -0500

    testing

--- JS/assign9script.js
+++ JS/assign9script.js
@@ -229,7 +229,7 @@ assign9.controller("assign9Ctrl", function ($scope, $http) {
             }
         });
         
-        window.addEventListener('mouseup', function (e) {
+        document.addEventListener('mouseup', function (e) {
             if (playheadClicked) {
                 alert("ended");
                 //    console.log("up");
