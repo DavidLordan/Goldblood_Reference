85cae71e6ad17160294636ce08f0c194b5165668
davidlordan <david_lordan@student.uml.edu>
2014-12-31 23:08:02 -0500

    testing

--- JS/assign9script.js
+++ JS/assign9script.js
@@ -192,10 +192,12 @@ assign9.controller("assign9Ctrl", function ($scope, $http) {
 
         progress.addEventListener('touchstart', function (e) {
             allowMouseUp = false;
+            
             playheadClicked = $scope.clickedPlayhead(e, this);
         });
         progress.addEventListener('mousedown', function (e) {
             allowMouseUp = true;
+            alert("mouseDown");
             console.log(allowMouseUp);
             playheadClicked = $scope.clickedPlayhead(e, this);
         });
