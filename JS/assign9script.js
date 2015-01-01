e48ba444b50c40749a1106895036ec2f53efd3d4
davidlordan <david_lordan@student.uml.edu>
2015-01-01 00:25:56 -0500

    testing

--- JS/assign9script.js
+++ JS/assign9script.js
@@ -227,7 +227,7 @@ assign9.controller("assign9Ctrl", function ($scope, $http) {
                 e.preventDefault();
                 // calculate the normalized position clicked
                 alert(e.pageX);
-                var clickPosition = ((e.pageX - this.offsetLeft) / this.offsetWidth);
+                var clickPosition = ((e.pageX - progress.offsetLeft) / progress.offsetWidth);
                 var clickTime = (clickPosition * myAudio.duration);
                 // move the playhead to the correct position
                 myAudio.currentTime = clickTime;
@@ -250,6 +250,7 @@ assign9.controller("assign9Ctrl", function ($scope, $http) {
                 var clickTime = (clickPosition * myAudio.duration);
                 // move the playhead to the correct position
                 myAudio.currentTime = clickTime;
+                alert(myAudio.currentTime + " " + clickTime);
                 //alert(clickTime);
                 bar.style.width = parseInt(((myAudio.currentTime / myAudio.duration) * 100), 10) + "%";
                 //alert("click1");
